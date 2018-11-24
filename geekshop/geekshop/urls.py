af65e5c20312b871eb9132b85ee03c990a7a1f41
Fillbary <p.sobako@yandex.ru>
2018-11-24 11:27:25 +0300

    YudinN_Django_lesson2HW

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -1,26 +1,30 @@
 """geekshop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-
-from mainapp import views as mainviews
+from django.contrib import admin
+import mainapp.views as mainapp
 
 urlpatterns = [
-    path('', mainviews.main),
-    path('products/', mainviews.products),
+    path('', mainapp.main, name="main"),
+    path('products/', mainapp.products, name="products"),
+    path('products/all', mainapp.products, name="products_all"),
+    path('products/home', mainapp.products, name="products_home"),
+    path('products/modern', mainapp.products, name="products_modern"),
+    path('products/office', mainapp.products, name="products_office"),
+    path('products/classic', mainapp.products, name="products_classic"),
+    path('contact/', mainapp.contact, name="contact"),
     path('admin/', admin.site.urls),
-    path('contact/', mainviews.contact),
 ]
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,14 +1,71 @@
 from django.shortcuts import render
 
 
-# Create your views here.
 def main(request):
-    return render(request, 'mainapp/index.html')
+    title = "главная"
+    products = [
+        {
+            'name': 'Отличный стул',
+            'disc': 'Расположитесь с комфортов',
+            'image_scr': 'product-1.jpg',
+            'image_href': '/product/1/',
+            'alt': 'продукт 1'
+        },
+        {
+            'name': 'Стул повышенного качества',
+            'disc': 'Не оторваться',
+            'image_scr': 'product-2.jpg',
+            'image_href': '/product/2/',
+            'alt': 'продукт 2'
+        }
+    ]
+    content = {
+        "title": title,
+        "products": products
+    }
 
+    return render(request, 'mainapp/index.html', content)
+    
 
 def products(request):
-    return render(request, 'mainapp/products.html')
-
+    title = "продукты"
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_classic', 'name': 'классика'}
+    ]
+    products = [
+        {
+            'name': 'Отличный стул',
+            'disc': 'Расположитесь с комфортов',
+            'image_scr': 'product-11.jpg',
+            'image_href': '/product/1/',
+            'alt': 'продукт 11'
+        },
+        {
+            'name': 'Стул повышенного качества',
+            'disc': 'Не оторваться',
+            'image_scr': 'product-21.jpg',
+            'image_href': '/product/2/',
+            'alt': 'продукт 21'
+        },
+        {
+            'name': 'Стул получше',
+            'disc': 'На нем не оторваться',
+            'image_scr': 'product-31.jpg',
+            'image_href': '/product/2/',
+            'alt': 'продукт 31'
+        }
+    ]
+    content = {
+        "title": title,
+        "links_menu": links_menu,
+        "products": products
+    }
+    return render(request, 'mainapp/products.html', content)
+    
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
