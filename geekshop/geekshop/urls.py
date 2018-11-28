5140d8893dbb86b0838366435cf5eb971815d6be
Fillbary <p.sobako@yandex.ru>
2018-11-28 07:38:03 +0300

    YudinN_Django_lesson3HW

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.urls import path
+
 import mainapp.views as mainapp
 
 urlpatterns = [
@@ -28,3 +31,6 @@ urlpatterns = [
     path('contact/', mainapp.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
